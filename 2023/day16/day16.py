26903d62252950c8c92d37e0ad49686a96d004ea
Martin Müller (302050) <martin.mueller@teambank.de>
2023-12-18 13:36:24 +0100

    day16

--- 2023/day16/day16.py
+++ 2023/day16/day16.py
@@ -20,51 +20,50 @@ TURN_RIGHT = {
 
 TURN_LEFT = {v: k for k, v in TURN_RIGHT.items()}
 
-all_empty = {tuple(x) for x in np.transpose(np.nonzero(cave_raw == '.'))}
-all_vertical_splitters = {tuple(x) for x in np.transpose(np.nonzero(cave_raw == '|'))}
-all_horizontal_splitters = {tuple(x) for x in np.transpose(np.nonzero(cave_raw == '-'))}
-all_left_right_mirrors = {tuple(x) for x in np.transpose(np.nonzero(cave_raw == '/'))}
-all_right_left_mirrors = {tuple(x) for x in np.transpose(np.nonzero(cave_raw == '\\'))}
 
-all_visited_position_direction = set()
-start_position = (0, -1)
-start_direction = (0, 1)
+def count_visited_tiles(start_position: POINT_TYP, start_direction: POINT_TYP):
+    all_visited_position_direction = set()
 
-
-def follow_beam(start: POINT_TYP, direction: POINT_TYP):
-    position = tuple_add(start, direction)
-    if not (0 <= position[0] < cave_shape[0] and 0 <= position[1] < cave_shape[1]):
-        return False
-    position_direction_tuple = (position, direction)
-    if position_direction_tuple in all_visited_position_direction:
-        return True
-    all_visited_position_direction.add(position_direction_tuple)
-    if position in all_empty:
-        follow_beam(position, direction)
-    elif direction[0] == 0:
-        if position in all_right_left_mirrors:
-            follow_beam(position, TURN_RIGHT[direction])
-        elif position in all_left_right_mirrors:
-            follow_beam(position, TURN_LEFT[direction])
-        elif position in all_vertical_splitters:
-            follow_beam(position, TURN_LEFT[direction])
-            follow_beam(position, TURN_RIGHT[direction])
-        elif position in all_horizontal_splitters:
+    def follow_beam(start: POINT_TYP, direction: POINT_TYP):
+        position = tuple_add(start, direction)
+        if not (0 <= position[0] < cave_shape[0] and 0 <= position[1] < cave_shape[1]):
+            return False
+        position_direction_tuple = (position, direction)
+        if position_direction_tuple in all_visited_position_direction:
+            return True
+        all_visited_position_direction.add(position_direction_tuple)
+        position_value = cave_raw[position]
+        if position_value == '.':
             follow_beam(position, direction)
-    elif direction[1] == 0:
-        if position in all_right_left_mirrors:
-            follow_beam(position, TURN_LEFT[direction])
-        elif position in all_left_right_mirrors:
-            follow_beam(position, TURN_RIGHT[direction])
-        elif position in all_horizontal_splitters:
-            follow_beam(position, TURN_LEFT[direction])
-            follow_beam(position, TURN_RIGHT[direction])
-        elif position in all_vertical_splitters:
-            follow_beam(position, direction)
-    else:
-        return False
+        elif direction[0] == 0:
+            if position_value == '\\':
+                follow_beam(position, TURN_RIGHT[direction])
+            elif position_value == '/':
+                follow_beam(position, TURN_LEFT[direction])
+            elif position_value == '|':
+                follow_beam(position, TURN_LEFT[direction])
+                follow_beam(position, TURN_RIGHT[direction])
+            elif position_value == '-':
+                follow_beam(position, direction)
+        elif direction[1] == 0:
+            if position_value == '\\':
+                follow_beam(position, TURN_LEFT[direction])
+            elif position_value == '/':
+                follow_beam(position, TURN_RIGHT[direction])
+            elif position_value == '-':
+                follow_beam(position, TURN_LEFT[direction])
+                follow_beam(position, TURN_RIGHT[direction])
+            elif position_value == '|':
+                follow_beam(position, direction)
+        else:
+            return False
+
+    follow_beam(start_position, start_direction)
+    return len({x for x, _ in all_visited_position_direction})
 
 
-follow_beam(start_position, start_direction)
-result = len({x for x, _ in all_visited_position_direction})
-print(result)
+print(count_visited_tiles((0, -1), (0, 1)))
+
+# part 2
+start_directions = ([t for x in range(cave_shape[1]) for t in [((-1, x), (1, 0)), ((cave_shape[0], x), (-1, 0))]] +
+                    [k for y in range(cave_shape[0]) for k in [((y, -1), (0, 1)), ((y, cave_shape[1]), (0, -1))]])
